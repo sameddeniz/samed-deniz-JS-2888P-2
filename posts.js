ee1f3aff4700e25f32c3de9147b92276f4efc86a
Samed Deniz <155846623+sameddeniz@users.noreply.github.com>
2024-04-09 03:30:16 +0300

    post page done

---
+++ posts.js
@@ -0,0 +1,30 @@
+async function fetchUserPosts() {
+  const postsFetched = new URLSearchParams(window.location.search);
+  const userId = postsFetched.get("id");
+  const apiURL = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+  const response = await fetch(apiURL);
+  const userPostData = await response.json();
+  displayUserPosts(userPostData);
+}
+
+function displayUserPosts(users) {
+  const postsContainer = document.getElementById("postsContainer");
+  users.forEach((user) => {
+    const userPostCard = `
+    <div class="col-md-12 text-center mb-3 mt-5">
+        <div class="card w-100">
+            <div class="card-body">
+            <h5 class="card-title">${user.id}</h5>
+            <div class="card-text">
+            <p class="card-text">${user.title}</p>
+            <p class="card-text">${user.body}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    `;
+    postsContainer.innerHTML += userPostCard;
+  });
+}
+
+fetchUserPosts();
--- users.js
+++ users.js
@@ -1,18 +1,18 @@
 async function fetchUsers() {
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const users = await response.json();
-      displayUsers(users);
-    } catch (error) {
-      console.log("Hata:", error);
-    }
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    displayUsers(users);
+  } catch (error) {
+    console.log("Hata:", error);
   }
-  
-  function displayUsers(usersdata) {
-    const container = document.getElementById("user-cards");
-    usersdata.forEach((user) => {
-      const userCardHTML = `
-      <div class="col-md-12 mb-4 d-flex align-items-stretch">
+}
+
+function displayUsers(usersdata) {
+  const container = document.getElementById("user-cards");
+  usersdata.forEach((user) => {
+    const userCardHTML = `
+      <div class="col-md-12 mb-4 d-flex align-items-stretch text-center">
       <div class="card w-100">
       <div class="card-body d-flex flex-column">
       <h5 class="card-title user-name">${user.name}</h5>
@@ -23,13 +23,14 @@ async function fetchUsers() {
       <p class="card-text"><i class="fa-solid fa-envelope"></i> ${user.email}</p>
       <p class="card-text"><i class="fa-solid fa-phone"></i> ${user.phone}</p>
       <p class="card-text"><i class="fa-solid fa-globe"></i> ${user.website}</p>
+      <a class="btn btn-primary" href="posts.html?id=${user.id}">View Posts</a>
       </div>
       </div>
       </div>
       </div>
       `;
-      container.innerHTML += userCardHTML;
-    });
-  }
-  
-  fetchUsers();
+    container.innerHTML += userCardHTML;
+  });
+}
+
+fetchUsers();
